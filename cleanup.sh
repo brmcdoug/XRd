03c75ca24c1d95f43032793deaeb8b0858751ccf
brmcdoug <brmcdoug@cisco.com>
2022-09-27 15:44:14 -0700

    initial commit

---
+++ cleanup.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+
+docker compose down
+docker volume rm xrd-25
+docker volume rm xrd-26
+docker volume rm xrd-27
+docker volume rm xrd-28
+docker volume rm xrd-29
+docker volume rm xrd-30
+docker volume rm xrd-31
+docker volume rm xrd-32
+docker volume rm xrd-33
+docker volume rm xrd-34

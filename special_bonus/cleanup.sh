55667f12353f89d035be61c851410268ca0e3db2
brmcdoug <brmcdoug@cisco.com>
2022-10-05 12:58:28 -0700

    fix yaml, cleanup

--- special_bonus/cleanup.sh
+++ special_bonus/cleanup.sh
@@ -1,9 +1,40 @@
 #/bin/sh
 
 docker-compose down
+docker volume rm xrd-00
+docker volume rm xrd-01
+docker volume rm xrd-02
+docker volume rm xrd-03
+docker volume rm xrd-04
+docker volume rm xrd-05
+docker volume rm xrd-06
+docker volume rm xrd-07
+docker volume rm xrd-08
+docker volume rm xrd-09
+docker volume rm xrd-10
+docker volume rm xrd-11
+docker volume rm xrd-12
+docker volume rm xrd-13
+docker volume rm xrd-14
+docker volume rm xrd-15
+docker volume rm xrd-16
+docker volume rm xrd-17
+docker volume rm xrd-18
+docker volume rm xrd-19
+docker volume rm xrd-20
+docker volume rm xrd-21
+docker volume rm xrd-22
+docker volume rm xrd-23
+docker volume rm xrd-24
+
 docker volume rm xrd-25
 docker volume rm xrd-26
 docker volume rm xrd-27
 docker volume rm xrd-28
 docker volume rm xrd-29
 docker volume rm xrd-30
+
+docker volume rm xrd-31
+docker volume rm xrd-32
+docker volume rm xrd-33
+docker volume rm xrd-34
